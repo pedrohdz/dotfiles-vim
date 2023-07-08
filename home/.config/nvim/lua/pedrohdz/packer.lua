5aa8a08829bb101981394bbdb830efec1264ab4c
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2023-07-08 15:46:23 +0200

    Auto bootstrap Packer

--- home/.config/nvim/lua/pedrohdz/packer.lua
+++ home/.config/nvim/lua/pedrohdz/packer.lua
@@ -1,3 +1,17 @@
+-- Bootstrap Packer if it is not already setup...
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -135,4 +149,8 @@ return require('packer').startup(function(use)
   use 'Glench/Vim-Jinja2-Syntax'
   use 'Rykka/InstantRst'
   use 'honza/vim-snippets'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
