f41c354beca20087204a0fc044e9e79371cb021d
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2023-10-26 10:47:36 +0200

    Upgraded indent-blankline to v3

--- home/.config/nvim/after/plugin/indent-blankline.lua
+++ home/.config/nvim/after/plugin/indent-blankline.lua
@@ -1,7 +1,21 @@
-require('indent_blankline').setup {
-  char = '┊',
-  space_char_blankline = ' ',
-  show_current_context = true,
-  show_current_context_start = true,
-  use_treesitter = true,
-}
+-- require('indent_blankline').setup {
+--   char = '┊',
+--   space_char_blankline = ' ',
+--   show_current_context = true,
+--   show_current_context_start = true,
+--   use_treesitter = true,
+-- }
+
+-- FIXME - Update v3 scope settings like v2 context settings.
+require('ibl').setup({
+  indent = {
+    char = '┊',
+    smart_indent_cap = true,
+  },
+  scope = {
+    enabled = true,
+    include = {
+      node_type = { ['*'] = {'*'}, },
+    },
+  },
+})
