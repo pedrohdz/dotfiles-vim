9a7950e0ae6ea62588ff09a5c4ce30ca5ba1852f
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2024-07-13 09:35:06 +0200

    Updated Oil

--- home/.config/nvim/after/plugin/oil.lua
+++ home/.config/nvim/after/plugin/oil.lua
@@ -4,7 +4,7 @@
 local oil = require('oil')
 
 oil.setup({
-  default_file_explorer = false,
+  default_file_explorer = true,
   silence_scp_warning = true,
   view_options = {
     show_hidden = true,
