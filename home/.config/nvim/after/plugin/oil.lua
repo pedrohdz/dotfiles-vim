5c6ec28286167e425ebdeab4f77c4c6d066d20d9
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2024-07-15 07:44:01 +0200

    Added minor Oil config

--- home/.config/nvim/after/plugin/oil.lua
+++ home/.config/nvim/after/plugin/oil.lua
@@ -5,7 +5,9 @@ local oil = require('oil')
 
 oil.setup({
   default_file_explorer = true,
+  delete_to_trash = true,
   silence_scp_warning = true,
+  skip_confirm_for_simple_edits = true,
   view_options = {
     show_hidden = true,
   },
