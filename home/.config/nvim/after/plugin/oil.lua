0246f5350df6c9bffac8a22a2d4959bf8c9c1a4b
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2024-09-20 14:50:26 +0200

    Updated the Oil config

--- home/.config/nvim/after/plugin/oil.lua
+++ home/.config/nvim/after/plugin/oil.lua
@@ -3,56 +3,124 @@
 -- ----------------------------------------------------------------------------
 local oil = require('oil')
 
--- TODO - Nice to have: Would be nice to get this to work with window-picker.
---  - https://github.com/stevearc/oil.nvim/issues/360#issuecomment-2099555989
---
--- local smart_split = function()
---   local entry = oil.get_cursor_entry()
---   local dir = oil.get_current_dir()
---   -- local current_window_id = vim.api.nvim_get_current_win()
---   local picked_window_id = require('window-picker').pick_window({
---     filter_rules = { include_current_win = false },
---   })
---
---   if picked_window_id == nil then
---     oil.close()
---     vim.cmd(string.format('vsplit %s/%s', dir, entry.name))
---   end
---   -- oil.select()
---   -- print(vim.inspect(entry))
---   -- print(dir)
---   -- print(picked_window_id)
--- end
 
+-- ----------------------------------------------------------------------------
+-- Helpers
+-- ----------------------------------------------------------------------------
+local is_always_hidden           = function(name, _)
+  return (
+    name:match([[~$]])
+    or name:match([[^%..*%.swp$]])
+    or name:match([[^%.terraform$]])
+    or name:match([[^%.terraform%.lock%.hcl$]])
+    or name:match([[^terraform%.tfstate$]])
+    or name:match([[^terraform%.tfstate%.backup$]])
+  )
+end
+
+-- Thank you:
+--  - https://github.com/stevearc/oil.nvim/issues/360#issuecomment-2099555989
+local window_picker              = function()
+  local entry_path = vim.fn.fnamemodify(
+    vim.fn.simplify(
+      vim.fn.expand(oil.get_current_dir() .. oil.get_cursor_entry().parsed_name)
+    ),
+    ':~:.'
+  )
+
+  local window_id = require('window-picker').pick_window({
+    filter_rules = {
+      autoselect_one = true,
+      include_current_win = true,
+    },
+  })
+
+  if not window_id then
+    print('Unable to pick window')
+    return
+  end
+
+  oil.close()
+  vim.api.nvim_set_current_win(window_id)
+  vim.cmd.edit(entry_path)
+end
+
+local _actions                   = {}
+
+_actions.help                    = {
+  'actions.show_help',
+  desc = 'Show help (<C-?>)'
+}
+
+_actions.select_vertical         = {
+  'actions.select',
+  opts = {
+    close = true,
+    vertical = true,
+  },
+  desc = 'Open in a right vertical split'
+}
+
+_actions.select_vertical_left    = {
+  'actions.select',
+  opts = {
+    close = true,
+    split = 'aboveleft',
+    vertical = true,
+  },
+  desc = 'Open in a left vertical split'
+}
+
+_actions.select_horizontal       = {
+  'actions.select',
+  opts = {
+    close = true,
+    horizontal = true,
+  },
+  desc = 'Open in a lower horizontal split'
+}
+
+_actions.select_horizontal_above = {
+  'actions.select',
+  opts = {
+    close = true,
+    horizontal = true,
+    split = 'aboveleft',
+  },
+  desc = 'Open in a upper horizontal split'
+}
+
+-- ----------------------------------------------------------------------------
+-- Configure
+-- ----------------------------------------------------------------------------
 oil.setup({
   default_file_explorer = true,
   delete_to_trash = true,
   silence_scp_warning = true,
   skip_confirm_for_simple_edits = true,
+
+  columns = {
+    'icon',
+    'type',
+    'permissions',
+    'size',
+    'mtime',
+  },
   keymaps = {
-    ["<C-s>"] = {
-      "actions.select",
-      opts = {
-        close = true,
-        vertical = true,
-      },
-      desc = "Open the entry in a vertical split"
-    },
-    -- ["<C-f>"] = {
-    --   smart_split
-    -- }
+    ['<C-s>'] = false,
+    ['<C-h>'] = false,
+    ['g?'] = false,
+
+    ['<M-?>'] = _actions.help,
+    ['<M-p>'] = window_picker,
+
+    ['<M-v>'] = _actions.select_vertical,
+    ['<M-V>'] = _actions.select_vertical_left,
+    ['<M-h>'] = _actions.select_horizontal,
+    ['<M-H>'] = _actions.select_horizontal_above,
   },
   view_options = {
     show_hidden = true,
-    is_always_hidden = function(name, _)
-      return (
-        name:match([[~$]])
-        or name:match([[^%..*%.swp$]])
-        or name:match([[^%.terraform$]])
-        or name:match([[^%.terraform%.lock%.hcl$]])
-        or name:match([[^terraform%.tfstate$]])
-        or name:match([[^terraform%.tfstate%.backup$]])
-      )
-    end
+    is_always_hidden = is_always_hidden
   },
 })
