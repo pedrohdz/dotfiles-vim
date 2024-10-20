c434e8967580b0c32c4685d1ebb75ae02f5fa0df
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2024-10-20 15:00:42 +0200

    Added todo items to status-line.lua

--- home/.config/nvim/after/plugin/status-line.lua
+++ home/.config/nvim/after/plugin/status-line.lua
@@ -7,4 +7,12 @@ require('lualine').setup({
 -- Cleaning up the status line
 vim.api.nvim_set_hl(0, 'WinSeparator', { guibg = nil })
 vim.opt.laststatus = 3
+
+-- TODO - Improve:
+--   - https://vi.stackexchange.com/questions/42003/what-does-vlua-mean-in-an-option
+--   - https://elianiva.my.id/posts/neovim-lua-statusline/
+--   - https://neovim.io/doc/user/options.html#'statusline' - Note:
+--      ```
+--      set statusline=%!MyStatusLine()
+--      ```
 vim.opt.winbar = '%=%m %f'
