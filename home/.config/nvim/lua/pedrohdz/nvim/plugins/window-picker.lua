400cbe3a5bed071821a777f8af21a641bc93810a
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2025-12-05 11:07:04 +0700

    Simplifies window-picker configuration

--- home/.config/nvim/lua/pedrohdz/nvim/plugins/window-picker.lua
+++ home/.config/nvim/lua/pedrohdz/nvim/plugins/window-picker.lua
@@ -1,12 +1,10 @@
 return {
   's1n7ax/nvim-window-picker',
   version = 'v2.*',
-  config = function()
-    require('window-picker').setup({
-      hint = 'floating-big-letter',
-      filter_rules = {
-        include_current_win = true,
-      },
-    })
-  end,
+  opts = {
+    hint = 'floating-big-letter',
+    filter_rules = {
+      include_current_win = true,
+    },
+  },
 }
