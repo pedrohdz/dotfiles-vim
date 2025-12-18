72d14ae331ad32f507c58445390ba910dc9229e5
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2025-12-18 17:35:38 +0700

    Added copilot.lua

---
+++ home/.config/nvim/lua/pedrohdz/nvim/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  'zbirenbaum/copilot.lua',
+  requires = {
+    'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+  },
+
+  config = true,
+
+  -- -- Lazy loading
+  -- lazy = true,
+  -- cmd = 'Copilot',
+  -- event = 'InsertEnter',
+}
