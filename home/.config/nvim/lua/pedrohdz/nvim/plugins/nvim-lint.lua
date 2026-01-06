35f3d2e6f8ecfa877f18a4852ddc7432c401a6c4
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2026-01-06 09:18:41 +0700

    Configured nvim-lint

--- home/.config/nvim/lua/pedrohdz/nvim/plugins/nvim-lint.lua
+++ home/.config/nvim/lua/pedrohdz/nvim/plugins/nvim-lint.lua
@@ -1,10 +1,41 @@
 -- Plugin from:
 --    https://github.com/mfussenegger/nvim-lint
+-- An asynchronous linter plugin for Neovim complementary to the built-in
+-- Language Server Protocol support.
 
--- TODO - Set this up...
+local function setup()
+  local lint = require('lint')
+
+  lint.linters_by_ft = {
+    ansible = { 'ansible-lint' },
+    bash = { 'bash', 'shellcheck' },
+    lua = { 'luacheck' },
+    nix = { 'nix' },
+    sh = { 'shellcheck' },
+    yaml = { 'yamllint' },
+    zsh = { 'zsh' },
+  }
+
+  local augroup = vim.api.nvim_create_augroup(
+    'phdz-lint',
+    { clear = true }
+  )
+  vim.api.nvim_create_autocmd(
+    {
+      'BufReadPost',
+      'BufWritePost',
+      'InsertLeave',
+    },
+    {
+      group = augroup,
+      callback = function()
+        lint.try_lint()
+        -- lint.try_lint('cspell')
+      end,
+    })
+end
 
 return {
   'mfussenegger/nvim-lint',
-
-  events = "LazyFile",
+  config = setup,
 }
